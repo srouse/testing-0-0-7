4ca7aac9702214cfd485758fa07b3bc9b21d1497
Scott Rouse <scott.rouse@gmail.com>
2023-11-07 13:52:23 -0600

    Create file: "transformations/fds-web/css-vars.d.ts"

---
+++ transformations/fds-web/css-vars.d.ts
@@ -0,0 +1,110 @@
+export type TSTTypeValue =
+  'type-test-h1' |
+  'type-test-h1-size' |
+  'type-test-h1-weight' |
+  'type-test-h2' |
+  'type-test-h2-size' |
+  'type-test-h2-weight' |
+  'type-test-h3' |
+  'type-test-h3-size' |
+  'type-test-h3-weight' |
+  'type-test-h4' |
+  'type-test-h4-size' |
+  'type-test-h4-weight' |
+  'type-test-h5' |
+  'type-test-h5-size' |
+  'type-test-h5-weight' |
+  'type-test-h6' |
+  'type-test-h6-size' |
+  'type-test-h6-weight' |
+  'type-test-p' |
+  'type-test-p-size' |
+  'type-test-p-weight' |
+  'type-test' |
+  'type-test-size' |
+  'type-test-weight';
+
+export type TSTColorValue =
+  'color-test-10' |
+  'color-test-20' |
+  'color-test-30' |
+  'color-test-40' |
+  'color-test-50' |
+  'color-test' |
+  'color-test-60' |
+  'color-test-70' |
+  'color-test-80' |
+  'color-test-90';
+
+export type TSTSpacingValue =
+  'spacing-col-0' |
+  'spacing-col-0-3' |
+  'spacing-col-0-4' |
+  'spacing-col-0-6' |
+  'spacing-col-0-8' |
+  'spacing-col-0-9' |
+  'spacing-col-1' |
+  'spacing-col-1-3' |
+  'spacing-col-1-4' |
+  'spacing-col-1-6' |
+  'spacing-col-1-8' |
+  'spacing-col-1-9' |
+  'spacing-col-2' |
+  'spacing-col-2-3' |
+  'spacing-col-2-4' |
+  'spacing-col-2-6' |
+  'spacing-col-2-8' |
+  'spacing-col-2-9' |
+  'spacing-col-3' |
+  'spacing-col-3-3' |
+  'spacing-col-3-4' |
+  'spacing-col-3-6' |
+  'spacing-col-3-8' |
+  'spacing-col-3-9' |
+  'spacing-col-4' |
+  'spacing-col-5' |
+  'spacing-col-6' |
+  'spacing-col-7' |
+  'spacing-col-8' |
+  'spacing-col-9' |
+  'spacing-col-10' |
+  'spacing-col-11' |
+  'spacing-col-12' |
+  'spacing-colvw-0' |
+  'spacing-colvw-0-3' |
+  'spacing-colvw-0-4' |
+  'spacing-colvw-0-6' |
+  'spacing-colvw-0-8' |
+  'spacing-colvw-0-9' |
+  'spacing-colvw-1' |
+  'spacing-colvw-1-3' |
+  'spacing-colvw-1-4' |
+  'spacing-colvw-1-6' |
+  'spacing-colvw-1-8' |
+  'spacing-colvw-1-9' |
+  'spacing-colvw-2' |
+  'spacing-colvw-2-3' |
+  'spacing-colvw-2-4' |
+  'spacing-colvw-2-6' |
+  'spacing-colvw-2-8' |
+  'spacing-colvw-2-9' |
+  'spacing-colvw-3' |
+  'spacing-colvw-3-3' |
+  'spacing-colvw-3-4' |
+  'spacing-colvw-3-6' |
+  'spacing-colvw-3-8' |
+  'spacing-colvw-3-9' |
+  'spacing-colvw-4' |
+  'spacing-colvw-5' |
+  'spacing-colvw-6' |
+  'spacing-colvw-7' |
+  'spacing-colvw-8' |
+  'spacing-colvw-9' |
+  'spacing-colvw-10' |
+  'spacing-colvw-11' |
+  'spacing-colvw-12';
+
+export type TSTValue =
+  TSTTypeValue |
+  TSTColorValue |
+  TSTSpacingValue;
